76edd9bf49d06bbfccffac0f20d767381dd82aa9
cuierd <cui.ding@uzh.ch>
2023-03-30 22:04:31 +0200

    change experimentID

--- experiments/motr-localCoherence/src/magpie.config.js
+++ experiments/motr-localCoherence/src/magpie.config.js
@@ -1,7 +1,7 @@
 export default {
-  experimentId: '3',
-  serverUrl: 'https://cui-test-3.herokuapp.com/',
-  socketUrl: 'wss://cui-test-3.herokuapp.com/socket',
+  experimentId: '4',
+  serverUrl: 'https://cui-motr-test.herokuapp.com/',
+  socketUrl: 'wss://cui-motr-test.herokuapp.com/socket',
   // this will be used in prolific mode
   completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1143PXS',
   contactEmail: 'cui.ding@uzh.ch',
