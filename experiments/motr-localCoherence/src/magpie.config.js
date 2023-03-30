6e67c1a11760e6cc4a0cef504c57a748e5806991
cuierd <cui.ding@uzh.ch>
2023-03-30 21:34:57 +0200

    change to another database

--- experiments/motr-localCoherence/src/magpie.config.js
+++ experiments/motr-localCoherence/src/magpie.config.js
@@ -1,7 +1,7 @@
 export default {
-  experimentId: '1',
-  serverUrl: 'https://gentle-everglades-00112.herokuapp.com/',
-  socketUrl: 'wss://gentle-everglades-00112.herokuapp.com/socket',
+  experimentId: '3',
+  serverUrl: 'https://cui-test-3.herokuapp.com/',
+  socketUrl: 'wss://cui-test-3.herokuapp.com/socket',
   // this will be used in prolific mode
   completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1143PXS',
   contactEmail: 'cui.ding@uzh.ch',
