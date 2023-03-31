1a9ce66806e63323933fbcc89d4ed1191193fe92
cuierd <cui.ding@uzh.ch>
2023-03-31 15:30:48 +0200

    try to add CORS

--- experiments/motr-localCoherence/src/main.js
+++ experiments/motr-localCoherence/src/main.js
@@ -12,6 +12,13 @@ Vue.use(VueKonva, { prefix: 'Canvas' });
 // Load magpie components...
 Vue.use(VueMagpie, magpieConfig);
 
+Vue.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // start app
 new Vue({
   render: (h) => h(App)
