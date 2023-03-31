e9c3000c25e633172516bd96294ae1e443627ffc
cuierd <cui.ding@uzh.ch>
2023-03-31 13:38:12 +0200

    try using http

--- experiments/motr-localCoherence/src/magpie.config.js
+++ experiments/motr-localCoherence/src/magpie.config.js
@@ -1,6 +1,6 @@
 export default {
   experimentId: '1',
-  serverUrl: 'https://cui-motr-new.herokuapp.com/',
+  serverUrl: 'http://cui-motr-new.herokuapp.com/',
   socketUrl: 'wss://cui-motr-new.herokuapp.com/socket',
   // this will be used in prolific mode
   completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1143PXS',
