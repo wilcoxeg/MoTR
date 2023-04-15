c0219a2bed2468ff66a507271e06995a6e2dccdb
cuierd <cui.ding@uzh.ch>
2023-04-15 16:42:24 +0200

    changed to gentle-ever database

--- experiments/motr-localCoherence/src/magpie.config.js
+++ experiments/motr-localCoherence/src/magpie.config.js
@@ -1,7 +1,7 @@
 export default {
   experimentId: '1',
-  serverUrl: 'https://cui-motr-new.herokuapp.com/',
-  socketUrl: 'wss://cui-motr-new.herokuapp.com/socket',
+  serverUrl: 'https://gentle-everglades-00112.herokuapp.com/',
+  socketUrl: 'wss://gentle-everglades-00112.herokuapp.com/socket',
   // this will be used in prolific mode
   completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1143PXS',
   contactEmail: 'cui.ding@uzh.ch',
